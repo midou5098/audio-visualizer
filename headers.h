8f9c18afd117a9ed37fe35b9ff729b7c2153fff3
mohamed amdouni <midouamdouni4@gmail.com>
2026-04-09 09:28:16 +0000

    basic window and reender system

--- headers.h
+++ headers.h
@@ -1,3 +1,72 @@
-#include <ostream>
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <vector>
+#include <iostream>
 #include <string>
-#include <SDL.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+
+
+
+
+
+
+
+
+class SDLinit{
+    private:
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+        TTF_Font* font;
+    public:
+        SDLinit(const std::string title,int w,int h);
+        void clear();
+        void present();
+        void handel(SDL_Event event);
+        void drawbut(int x,int y,int w,int h,int r,int g,int b,const std::string &text);
+
+
+};
+
+SDLinit::SDLinit(const std::string title,int w,int h){
+    SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO);
+    window=SDL_CreateWindow(title.c_str(),SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,w,h,SDL_WINDOW_SHOWN);
+    renderer=SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+    TTF_Init();
+    TTF_OpenFont("font.ttf",20);
+    
+
+}
+void SDLinit::clear(){
+    if (renderer){
+        SDL_SetRenderDrawColor(renderer,0,0,0,255);
+        SDL_RenderClear(renderer);
+    }
+}
+void SDLinit::present(){
+    if(renderer){
+        SDL_RenderPresent(renderer);
+    }
+}
+
+
+void SDLinit::drawbut(int x,int y,int w,int h,int r,int g,int b,const std::string &text){
+    
+    SDL_SetRenderDrawColor(renderer,r,g,b,255);
+    SDL_Rect rect6={x,y,w,h};
+    SDL_RenderFillRect(renderer,&rect6);
+    SDL_SetRenderDrawColor(renderer,0,0,0,255);
+    SDL_RenderDrawRect(renderer,&rect6);
+    SDL_Color white = {120,120,120,255};
+    SDL_Surface* surf=TTF_RenderText_Solid(font,text.c_str(),white);
+    SDL_Texture* tex=SDL_CreateTextureFromSurface(renderer,surf);
+    SDL_Rect rect5={x+27,y+10,w-50,h-30};
+    SDL_RenderCopy(renderer,tex,NULL,&rect5);
+    SDL_FreeSurface(surf);
+    SDL_DestroyTexture(tex);
+    
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
 #include "headers.h"
+
+
+
+
+
+int main(){
+    SDLinit sdl("fuckass visualizer" , 1280,720);
+    bool running =true;
+    SDL_Event event;
+    while(running){
+        while(SDL_PollEvent(&event)){
+            if(event.type==SDL_QUIT){
+                SDL_Quit();
+            }
+            sdl.clear();
+            sdl.present();
+
+        }
+    }
+    return 0;
+}
